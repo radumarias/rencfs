c6a336cce041b0ae45aaf83e3e20fd776506df42
chr.m <chri.marios@gmail.com>
2024-08-19 22:25:26 +0300

    Add unit tests for crypto module

--- src/crypto.rs
+++ src/crypto.rs
@@ -3,6 +3,7 @@ use std::io;
 use std::io::{Read, Seek, Write};
 use std::num::ParseIntError;
 use std::path::{Path, PathBuf};
+use std::str::FromStr;
 
 use argon2::Argon2;
 use base64::alphabet::STANDARD;
@@ -237,15 +238,19 @@ pub fn encrypt_file_name(
     cipher: Cipher,
     key: &SecretVec<u8>,
 ) -> FsResult<String> {
-    if name.expose_secret() == "$." || name.expose_secret() == "$.." {
-        Ok(name.expose_secret().clone())
-    } else if name.expose_secret() == "." || name.expose_secret() == ".." {
-        Ok(format!("${}", name.expose_secret()))
-    } else {
-        let normalized_name = SecretString::new(name.expose_secret().replace(['/', '\\'], " "));
-        let mut encrypted = encrypt(&normalized_name, cipher, key)?;
-        encrypted = encrypted.replace('/', "|");
-        Ok(encrypted)
+    let secret_string = name.expose_secret();
+
+    match secret_string.as_str() {
+        "$." | "$.." => Ok(secret_string.clone()),
+        "." | ".." => Ok(format!("${secret_string}")),
+        _ => {
+            let secret = SecretString::from_str(secret_string)
+                .map_err(|err| Error::GenericString(err.to_string()))?;
+            let mut encrypted = encrypt(&secret, cipher, key)?;
+            encrypted = encrypted.replace('/', "|");
+
+            Ok(encrypted)
+        }
     }
 }
 
@@ -372,4 +377,276 @@ where
     file.commit()?;
     File::open(parent)?.sync_all()?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use rand_core::RngCore;
+    use secrecy::{ExposeSecret, Secret, SecretString, SecretVec};
+    use std::{
+        fs::File,
+        io::{self, Write},
+        path::{Path, PathBuf},
+    };
+    use tempfile::{tempdir, TempDir};
+
+    fn create_encrypted_file(
+        content: &str,
+        cipher: Cipher,
+        key: &Secret<Vec<u8>>,
+    ) -> (TempDir, PathBuf) {
+        let temp_dir = tempdir().unwrap();
+        let file_path = temp_dir.path().join("test_file.txt");
+
+        let mut file = File::create(file_path.clone()).unwrap();
+        file.write_all(content.as_bytes()).unwrap();
+        file.flush().unwrap();
+
+        let encrypted_file_path = Path::new(&file_path).to_path_buf().with_extension("enc");
+        if encrypted_file_path.exists() {
+            std::fs::remove_file(&encrypted_file_path).unwrap();
+        }
+
+        let mut file = File::open(file_path).unwrap();
+        let mut writer = create_write(
+            File::create(encrypted_file_path.clone()).unwrap(),
+            cipher,
+            key,
+        );
+        io::copy(&mut file, &mut writer).unwrap();
+        writer.finish().unwrap();
+
+        (temp_dir, encrypted_file_path)
+    }
+
+    fn secret_key(cipher: Cipher) -> Secret<Vec<u8>> {
+        let mut key = vec![0; cipher.key_len()];
+        create_rng().fill_bytes(&mut key);
+        SecretVec::new(key)
+    }
+
+    #[test]
+    fn test_simple_encrypt_and_decrypt() {
+        let secret = SecretString::new("Test secret".to_string());
+
+        for &cipher in &[Cipher::ChaCha20Poly1305, Cipher::Aes256Gcm] {
+            let key = secret_key(cipher);
+
+            let encrypted = encrypt(&secret, cipher, &key).unwrap();
+            let decrypted = decrypt(&encrypted, cipher, &key).unwrap();
+            assert_eq!(decrypted.expose_secret(), secret.expose_secret());
+        }
+    }
+
+    #[test]
+    fn test_encrypt_and_decrypt_file_name() {
+        let secret_name = SecretString::new("testfile.txt".to_string());
+
+        for &cipher in &[Cipher::ChaCha20Poly1305, Cipher::Aes256Gcm] {
+            let key = secret_key(cipher);
+            let encrypted = encrypt_file_name(&secret_name, cipher, &key).unwrap();
+            let decrypted = decrypt_file_name(&encrypted, cipher, &key).unwrap();
+            assert_eq!(decrypted.expose_secret(), secret_name.expose_secret());
+        }
+
+        let secret_name = SecretString::new("testfile\\With/slash.txt".to_string());
+
+        for &cipher in &[Cipher::ChaCha20Poly1305, Cipher::Aes256Gcm] {
+            let key = secret_key(cipher);
+            let encrypted = encrypt_file_name(&secret_name, cipher, &key).unwrap();
+            let decrypted = decrypt_file_name(&encrypted, cipher, &key).unwrap();
+            assert_eq!(decrypted.expose_secret(), secret_name.expose_secret());
+        }
+    }
+
+    #[test]
+    fn test_encrypt_and_decrypt_file_name_invalid_cipher() {
+        let key = secret_key(Cipher::ChaCha20Poly1305);
+        let secret_name = SecretString::new("testfile.txt".to_string());
+
+        let encrypted = encrypt_file_name(&secret_name, Cipher::ChaCha20Poly1305, &key).unwrap();
+        let result = decrypt_file_name(&encrypted, Cipher::Aes256Gcm, &key);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_derive_key() {
+        let password = SecretString::new("password".to_string());
+        let salt = b"salt_of_pass";
+
+        for &cipher in &[Cipher::ChaCha20Poly1305, Cipher::Aes256Gcm] {
+            let derived_key = derive_key(&password, cipher, salt).unwrap();
+            assert_eq!(derived_key.expose_secret().len(), cipher.key_len());
+        }
+    }
+
+    #[test]
+    fn test_derive_key_consistency() {
+        let password = SecretString::new("password".to_string());
+        let salt = b"random_salt";
+
+        let derived_key_1 = derive_key(&password, Cipher::ChaCha20Poly1305, salt).unwrap();
+        let derived_key_2 = derive_key(&password, Cipher::ChaCha20Poly1305, salt).unwrap();
+
+        assert_eq!(derived_key_1.expose_secret(), derived_key_2.expose_secret());
+    }
+
+    #[test]
+    fn test_derive_key_empty_salt() {
+        let empty_password = SecretString::new("password".to_string());
+        let empty_salt = b"";
+
+        let result = derive_key(&empty_password, Cipher::ChaCha20Poly1305, empty_salt);
+
+        // Salt is too small
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_derive_key_uniqueness() {
+        let password = SecretString::new("password".to_string());
+        let salts = vec![b"random_salt1", b"random_salt2", b"random_salt3"];
+
+        let mut derived_keys = std::collections::HashSet::new();
+        for salt in salts.clone() {
+            let derived_key = derive_key(&password, Cipher::ChaCha20Poly1305, salt).unwrap();
+            derived_keys.insert(derived_key.expose_secret().clone());
+        }
+
+        assert_eq!(derived_keys.len(), salts.len());
+    }
+
+    #[test]
+    fn test_encrypt_decrypt() {
+        for &cipher in &[Cipher::ChaCha20Poly1305, Cipher::Aes256Gcm] {
+            let key = secret_key(cipher);
+
+            let data = SecretString::new("A".to_string());
+            let encrypted = encrypt(&data, cipher, &key).unwrap();
+            let decrypted = decrypt(&encrypted, cipher, &key).unwrap();
+            assert_eq!(decrypted.expose_secret(), data.expose_secret());
+
+            let large_data = SecretString::new("A".repeat(1024 * 1024)); // 1 MB
+            let encrypted = encrypt(&large_data, cipher, &key).unwrap();
+            let decrypted = decrypt(&encrypted, cipher, &key).unwrap();
+            assert_eq!(decrypted.expose_secret(), large_data.expose_secret());
+        }
+    }
+
+    #[test]
+    fn test_encrypt_decrypt_empty_string() {
+        let key = SecretVec::new(vec![0; 32]);
+        let secret = SecretString::new("".to_string());
+
+        let encrypted = encrypt(&secret, Cipher::ChaCha20Poly1305, &key).unwrap();
+        let decrypted = decrypt(&encrypted, Cipher::ChaCha20Poly1305, &key).unwrap();
+
+        assert_eq!(decrypted.expose_secret(), "");
+    }
+
+    #[test]
+    fn test_hash_file_name_special_cases() {
+        let expected = "$.".to_string();
+        let name = SecretString::new(expected.clone());
+        let result = hash_file_name(&name);
+        assert_eq!(result, expected);
+
+        let expected = "$..".to_string();
+        let name = SecretString::new(expected.clone());
+        let result = hash_file_name(&name);
+        assert_eq!(result, expected);
+
+        let input = ".".to_string();
+        let expected = "$.".to_string();
+        let name = SecretString::new(input);
+        let result = hash_file_name(&name);
+        assert_eq!(result, expected);
+
+        let input = "..".to_string();
+        let expected = "$..".to_string();
+        let name = SecretString::new(input);
+        let result = hash_file_name(&name);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_hash_file_name_regular_case() {
+        let name = SecretString::new("filename.txt".to_string());
+        let result = hash_file_name(&name);
+        let expected_hash = hex::encode(hash_secret_string(&name));
+        assert_eq!(result, expected_hash);
+    }
+
+    #[test]
+    fn test_hash_secret_string() {
+        let secret = SecretString::new("hash this secret".to_string());
+        let expected_hash_hex = "d820cbf278fc742d8ec30e43947674689cd06d5aa9b71a2f9afe162a4ce408dc";
+
+        let hash_hex = hex::encode(hash_secret_string(&secret));
+        assert_eq!(hash_hex, expected_hash_hex);
+    }
+
+    #[test]
+    fn test_copy_from_file_exact() {
+        let cipher = Cipher::ChaCha20Poly1305;
+        let key = secret_key(cipher);
+
+        let content = "Hello World!";
+        let (_temp_dir, file_path) = create_encrypted_file(content, cipher, &key);
+
+        let mut output = Vec::new();
+        copy_from_file_exact(
+            file_path.clone(),
+            0,
+            content.len() as u64,
+            cipher,
+            &key,
+            &mut output,
+        )
+        .unwrap();
+        assert_eq!(&output, content.as_bytes());
+    }
+
+    #[test]
+    fn test_copy_from_file_exact_zero_length() {
+        let key = SecretVec::new(vec![0; 32]);
+        let (_temp_dir, file_path) =
+            create_encrypted_file("Hello, world!", Cipher::ChaCha20Poly1305, &key);
+
+        let mut output = Vec::new();
+        let result = copy_from_file_exact(
+            file_path.clone(),
+            0,
+            0,
+            Cipher::ChaCha20Poly1305,
+            &key,
+            &mut output,
+        );
+
+        assert!(result.is_ok());
+        assert!(output.is_empty());
+    }
+
+    #[test]
+    fn test_copy_from_file_exact_position_beyond_eof() {
+        let cipher = Cipher::ChaCha20Poly1305;
+        let key = secret_key(cipher);
+
+        let content = "Hello, world!";
+        let (_temp_dir, file_path) = create_encrypted_file(content, Cipher::ChaCha20Poly1305, &key);
+
+        let mut output = Vec::new();
+        let result = copy_from_file_exact(
+            file_path.clone(),
+            content.len() as u64 + 1,
+            10,
+            Cipher::ChaCha20Poly1305,
+            &key,
+            &mut output,
+        );
+
+        assert!(result.is_err());
+    }
 }

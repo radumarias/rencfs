f18e87a60ea6b0dd2b8758e7e48a0f8e3510063a
Radu Marias <radumarias@gmail.com>
2024-08-31 16:30:02 +0300

    Update check-before-push.sh

--- check-before-push.sh
+++ check-before-push.sh
@@ -8,38 +8,38 @@ export RUSTDOCFLAGS="-Dwarnings"
 
 cargo fmt --all
 
-cargo build --all-targets --all-features --target x86_64-unknown-linux-gnu
-cargo build --release --all-targets --all-features --target x86_64-unknown-linux-gnu
-cargo clippy --release --all-targets --fix --allow-dirty --target x86_64-unknown-linux-gnu
+cargo build --all-targets --all-features
+cargo build --release --all-targets --all-features
+cargo clippy --release --all-targets --fix --allow-dirty
 cargo fmt --all -- --check
-cargo check --all --target x86_64-unknown-linux-gnu
-cargo clippy --all-targets --release --target x86_64-unknown-linux-gnu -- \
+cargo check --all
+cargo clippy --all-targets --release -- \
     -A clippy::similar_names \
     -A clippy::too_many_arguments \
     -A clippy::significant_drop_tightening \
     -A clippy::redundant_closure \
     -A clippy::missing_errors_doc \
     -A clippy::type_complexity
-cargo test --release --all --all-features --target x86_64-unknown-linux-gnu
-cargo doc --workspace --all-features --no-deps --target x86_64-unknown-linux-gnu
-cargo publish --dry-run --allow-dirty --target x86_64-unknown-linux-gnu
+cargo test --release --all --all-features
+cargo doc --workspace --all-features --no-deps
+cargo publish --dry-run --allow-dirty
 cargo aur
 cargo generate-rpm
 
 cd java-bridge
 cargo fmt --all
-cargo build --all-targets --all-features --target x86_64-unknown-linux-gnu
-cargo build --release --all-targets --all-features --target x86_64-unknown-linux-gnu
-cargo clippy --release --all-targets --fix --allow-dirty --target x86_64-unknown-linux-gnu
+cargo build --all-targets --all-features
+cargo build --release --all-targets --all-features
+cargo clippy --release --all-targets --fix --allow-dirty
 cargo fmt --all -- --check
-cargo check --all --target x86_64-unknown-linux-gnu
-cargo clippy --all-targets --release --target x86_64-unknown-linux-gnu -- \
+cargo check --all
+cargo clippy --all-targets --release -- \
     -A clippy::similar_names \
     -A clippy::too_many_arguments \
     -A clippy::significant_drop_tightening \
     -A clippy::redundant_closure \
     -A clippy::missing_errors_doc \
     -A clippy::type_complexity
-cargo test --release --all --all-features --target x86_64-unknown-linux-gnu
-cargo doc --workspace --all-features --no-deps --target x86_64-unknown-linux-gnu
+cargo test --release --all --all-features
+cargo doc --workspace --all-features --no-deps
 cd ..

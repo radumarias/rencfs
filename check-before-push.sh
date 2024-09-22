b5a26f3aae06e116f641710720e41fb938dcad01
Radu Marias <radumarias@gmail.com>
2024-09-22 22:39:02 +0300

    don't use act for now

--- check-before-push.sh
+++ check-before-push.sh
@@ -8,4 +8,35 @@ export RUSTDOCFLAGS="-Dwarnings"
 
 cargo fmt --all
 cargo clippy --release --all-targets --fix --allow-dirty --allow-staged
-act --action-offline-mode -W .github/workflows/build_and_tests_reusable.yaml
+
+cargo build --all-targets --all-features
+cargo build --release --all-targets --all-features
+cargo fmt --all -- --check
+cargo check --all
+cargo clippy --all-targets --release -- \
+  -A clippy::similar_names \
+  -A clippy::too_many_arguments \
+  -A clippy::significant_drop_tightening \
+  -A clippy::redundant_closure \
+  -A clippy::missing_errors_doc \
+  -A clippy::type_complexity
+cargo doc --workspace --all-features --no-deps
+cargo test --release --all --all-features
+cargo publish --dry-run --allow-dirty
+cargo aur
+cargo generate-rpm
+
+cd java-bridge
+cargo build --all-targets --all-features
+cargo build --release --all-targets --all-features
+cargo fmt --all -- --check
+cargo check --all
+cargo clippy --all-targets --release -- \
+  -A clippy::similar_names \
+  -A clippy::too_many_arguments \
+  -A clippy::significant_drop_tightening \
+  -A clippy::redundant_closure \
+  -A clippy::missing_errors_doc \
+  -A clippy::type_complexity
+cargo doc --workspace --all-features --no-deps
+cargo test --release --all --all-features
